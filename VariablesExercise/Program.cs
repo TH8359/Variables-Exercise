f883ad5c340ede6a2f09eb0325c4b84763f27b53
TH8359 <hogue73@embarqmail.com>
2025-08-26 00:15:23 -0400

    made variables & message

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string friendName = "Ben";
+            int newAge = 11;
+            char firstLetter = 'B';
+            bool confirmation = true;
+            double cake = 19.99;
+            decimal candles = 4.99m;
+
+            Console.WriteLine($"I have a friend named {friendName}. At 8:00 PM, I asked him via text if it was his birthday today. He texted, \"That Is {confirmation}\". Apparently, he turned {newAge} today. According to his family, the cake cost ${cake} and the candles cost ${candles}. For the writing, they decided to put a big {firstLetter} on it.");
         }
     }
 }
